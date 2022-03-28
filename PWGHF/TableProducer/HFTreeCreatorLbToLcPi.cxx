3871ce310feb77a5d7a2eed2cc96913befeef038
mjongerh <mjongerh@live.nl>
2022-03-28 15:33:01 +0200

    s

--- PWGHF/TableProducer/HFTreeCreatorLbToLcPi.cxx
+++ PWGHF/TableProducer/HFTreeCreatorLbToLcPi.cxx
@@ -269,20 +269,12 @@ struct HfTreeCreatorLbToLcPi {
            auto RICHTrk3Pi = -5000.0;
            auto RICHTrk3p = -5000.0;
 
-
-           /*if (track0.has_rich())
-if (track1.has_rich())
-if (track1.has_rich())
-if (track2.has_rich())
-if (track3.has_rich())
-if (track3.has_rich())
-*/
-          RICHPi0 = track0.rich().richNsigmaPi();
-          RICHTrk1Pi = track1.rich().richNsigmaPi();
-          RICHTrk1p = track1.rich().richNsigmaPr();
-          RICHTrk2K = track2.rich().richNsigmaKa();
-          RICHTrk3Pi = track3.rich().richNsigmaPi();
-          RICHTrk3p = track3.rich().richNsigmaPr();
+          if (track0.has_rich()) RICHPi0 = track0.rich().richNsigmaPi();
+          if (track1.has_rich()) RICHTrk1Pi = track1.rich().richNsigmaPi();
+          if (track1.has_rich()) RICHTrk1p = track1.rich().richNsigmaPr();
+          if (track2.has_rich()) RICHTrk2K = track2.rich().richNsigmaKa();
+          if (track3.has_rich()) RICHTrk3Pi = track3.rich().richNsigmaPi();
+          if (track3.has_rich()) RICHTrk3p = track3.rich().richNsigmaPr();
 
           rowCandidateFull(
             candidate.rSecondaryVertex(),
